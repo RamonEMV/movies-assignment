2894af2a1b989ac210019785d485c71483627265
RamonEMV <ramon_melchorzx@hotmail.com>
2023-07-17 22:13:04 -0600

    implement genres context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import TopBar from "@/components/TopBar";
-import { getMoviesListing } from "../../services/MovieServices";
-import { useState, useEffect } from "react";
+import { getMoviesListing } from "@/services/MovieServices";
+import { useState, useEffect, createContext } from "react";
 import useSWRInfinite from "swr/infinite";
 import MoviesList from "@/components/list/MoviesList";
-import { IMovie, IMoviesResponse } from "@/interfaces/Interfaces";
+import { IGenre, IMovie, IMoviesResponse } from "@/interfaces/Interfaces";
+import useSWR from "swr";
+import { getMovieGenres } from "@/services/GenreServices";
+import GenresContext from "@/context/genresContext";
 
 export default function Home() {
   const [isInfiniteScrollActive, setIsInfiniteScrollActive] =
@@ -15,6 +18,8 @@ export default function Home() {
     (index: number) => ({ criteria: sortingCriteria, page: index + 1 }),
     getMoviesListing
   );
+
+  const { data: genresData } = useSWR("/", getMovieGenres);
 
   const dataPlaceholder: IMoviesResponse[] = [];
   const jointData = data ? dataPlaceholder.concat(...data) : [];
@@ -39,40 +44,42 @@ export default function Home() {
   }, [size]);
 
   return (
-    <div className="w-screen flex flex-col items-center">
-      <TopBar />
-      <div className="w-9/12 max-w-7xl py-6">
-        <h2 className="text-2xl font-semibold">Popular movies</h2>
-        <button onClick={() => setSize(size + 1)}>Load More</button>
-        <div className="w-full flex">
-          <div className="flex-1 flex">
-            <button onClick={() => setSetsortingCriteria("popular")}>
-              Popular
-            </button>
-            <button onClick={() => setSetsortingCriteria("upcoming")}>
-              Upcoming
-            </button>
-          </div>
-          <div className="">
-            {!data || isLoading ? (
-              <div>Loading</div>
-            ) : (
-              <div className="flex flex-col">
-                <MoviesList movies={moviesList} />
-                <button
-                  onClick={() => {
-                    setSize(size + 1);
-                    setIsInfiniteScrollActive(true);
-                  }}
-                  className="h-12 w-full text-3xl font-bold text-white bg-sky-500 my-4 rounded-xl"
-                >
-                  Load more
-                </button>
-              </div>
-            )}
+    <GenresContext.Provider value={genresData}>
+      <div className="w-screen flex flex-col items-center">
+        <TopBar />
+        <div className="w-9/12 max-w-7xl py-6">
+          <h2 className="text-2xl font-semibold">Popular movies</h2>
+          <button onClick={() => setSize(size + 1)}>Load More</button>
+          <div className="w-full flex">
+            <div className="flex-1 flex">
+              <button onClick={() => setSetsortingCriteria("popular")}>
+                Popular
+              </button>
+              <button onClick={() => setSetsortingCriteria("upcoming")}>
+                Upcoming
+              </button>
+            </div>
+            <div className="">
+              {!data || isLoading ? (
+                <div>Loading</div>
+              ) : (
+                <div className="flex flex-col">
+                  <MoviesList movies={moviesList} />
+                  <button
+                    onClick={() => {
+                      setSize(size + 1);
+                      setIsInfiniteScrollActive(true);
+                    }}
+                    className="h-12 w-full text-3xl font-bold text-white bg-sky-500 my-4 rounded-xl"
+                  >
+                    Load more
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </GenresContext.Provider>
   );
 }
--- src/components/list/MovieListItem.tsx
+++ src/components/list/MovieListItem.tsx
@@ -1,9 +1,10 @@
-import { FunctionComponent } from "react";
-import { IMovie } from "../../interfaces/Interfaces";
+import { FunctionComponent, useContext } from "react";
+import { IGenre, IMovie } from "../../interfaces/Interfaces";
 import VotesMarker from "@/components/VotesMarker/VotesMarker";
 import { EllipsisHorizontalCircleIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import moment from "moment";
+import GenresContext from "@/context/genresContext";
 
 interface MovieListItemProps {
   movie: IMovie;
@@ -11,6 +12,17 @@ interface MovieListItemProps {
 
 const MovieListItem: FunctionComponent<MovieListItemProps> = ({ movie }) => {
   const scorePercentage = movie.vote_average * 10;
+
+  const genresContext = useContext(GenresContext);
+
+  const genresList = movie.genre_ids.map((genreId, index) => {
+    if (!genresContext?.genres) return;
+    const name = genresContext.genres.find(
+      (genre: IGenre) => genreId === genre.id
+    )?.name;
+    if (index === movie.genre_ids.length - 1) return name;
+    return `${name}, `;
+  });
 
   return (
     <div className="flex overflow-hidden flex-col rounded-xl h-96 w-44 drop-shadow-md bg-white">
@@ -23,20 +35,21 @@ const MovieListItem: FunctionComponent<MovieListItemProps> = ({ movie }) => {
         <img
           src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${movie.poster_path}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`}
           alt={`${movie.title} poster`}
+          className="w-48"
         />
       </Link>
 
-      <div className="p-2 pt-6">
+      <div className="p-2 pt-5">
         <div className="absolute h-10 w-10 bottom-24">
           <VotesMarker percentage={scorePercentage} />
         </div>
         <Link href={`/movie/${movie.id}`}>
-          <p className="font-bold">{movie.title}</p>
+          <p className="font-bold line-clamp-2">{movie.title}</p>
         </Link>
-        <p className="text-gray-500 font-normal">
+        <p className="text-gray-500 font-normal text-sm">
           {moment(movie.release_date).format("MMM DD[, ]YYYY")}
         </p>
-        <p>{movie.genre_ids.map((id) => id + " ")}</p>
+        <p className="text-xs ml-1 text-gray-500">{genresList}</p>
       </div>
     </div>
   );
