6c30f743a3d623e9158aaf684e445a6c6ac67530
RamonEMV <ramon_melchorzx@hotmail.com>
2023-07-19 18:30:32 -0600

    implement filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
 "use client";
 import TopBar from "@/components/TopBar";
 import { getMoviesListing } from "@/services/MovieServices";
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect } from "react";
 import useSWRInfinite from "swr/infinite";
 import MoviesList from "@/components/list/MoviesList";
-import { IGenre, IMovie, IMoviesResponse } from "@/interfaces/Interfaces";
+import {
+  IMovie,
+  IMoviesListingParams,
+  IMoviesResponse,
+  SortingCriteria,
+} from "@/interfaces/Interfaces";
 import useSWR from "swr";
 import { getMovieGenres } from "@/services/GenreServices";
 import GenresContext from "@/context/genresContext";
+import "rc-slider/assets/index.css";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import FiltersPanel from "@/components/list/FiltersPanel";
 
 export default function Home() {
   const [isInfiniteScrollActive, setIsInfiniteScrollActive] =
     useState<boolean>(false);
-  const [sortingCriteria, setSetsortingCriteria] = useState<any>("popular");
+  const [sortingCriteria, setSetsortingCriteria] = useState<SortingCriteria>(
+    SortingCriteria.POPULAR
+  );
+  const [fromDateFilter, setFromDateFilter] = useState<string | undefined>(
+    undefined
+  );
+  const [toDateFilter, setToDateFilter] = useState<string | undefined>(
+    undefined
+  );
+  const [selectedGenres, setSetselectedGenres] = useState<
+    number[] | undefined
+  >();
+  const [userScoreFilter, setUserScoreFilter] = useState<number[] | undefined>(
+    undefined
+  );
+  const [runtimeFilter, setRuntimeFilter] = useState<number[] | undefined>(
+    undefined
+  );
+  const [searchFilters, setSearchFilters] =
+    useState<IMoviesListingParams | null>({
+      criteria: SortingCriteria.POPULAR,
+    });
 
   const { data, size, setSize, isLoading } = useSWRInfinite(
-    (index: number) => ({ criteria: sortingCriteria, page: index + 1 }),
+    (index: number) => ({ ...searchFilters, page: index + 1 }),
     getMoviesListing
   );
 
@@ -29,6 +58,15 @@ export default function Home() {
     []
   );
 
+  const onSubmitSearch = (params: IMoviesListingParams) => {
+    setSearchFilters(params);
+  };
+
+  const onPressNextPage = () => {
+    setSize(size + 1);
+    setIsInfiniteScrollActive(true);
+  };
+
   useEffect(() => {
     const handleScroll = (e: Event) => {
       const scrollableHeight =
@@ -41,7 +79,7 @@ export default function Home() {
     return () => {
       document.removeEventListener("scroll", handleScroll);
     };
-  }, [size]);
+  }, [onPressNextPage]);
 
   return (
     <GenresContext.Provider value={genresData}>
@@ -49,31 +87,45 @@ export default function Home() {
         <TopBar />
         <div className="w-9/12 max-w-7xl py-6">
           <h2 className="text-2xl font-semibold">Popular movies</h2>
-          <button onClick={() => setSize(size + 1)}>Load More</button>
           <div className="w-full flex">
-            <div className="flex-1 flex">
-              <button onClick={() => setSetsortingCriteria("popular")}>
-                Popular
-              </button>
-              <button onClick={() => setSetsortingCriteria("upcoming")}>
-                Upcoming
-              </button>
-            </div>
-            <div className="">
+            <FiltersPanel
+              fromDateFilter={fromDateFilter}
+              genresData={genresData}
+              runtimeFilter={runtimeFilter}
+              selectedGenres={selectedGenres}
+              sortingCriteria={sortingCriteria}
+              toDateFilter={toDateFilter}
+              userScoreFilter={userScoreFilter}
+              setFromDateFilter={setFromDateFilter}
+              setRuntimeFilter={setRuntimeFilter}
+              setSetselectedGenres={setSetselectedGenres}
+              setSetsortingCriteria={setSetsortingCriteria}
+              setToDateFilter={setToDateFilter}
+              setUserScoreFilter={setUserScoreFilter}
+              onSubmitSearch={onSubmitSearch}
+            />
+            <div className="w-2/3">
               {!data || isLoading ? (
-                <div>Loading</div>
+                <div className="w-full h-full">
+                  <LoadingSpinner />
+                </div>
               ) : (
-                <div className="flex flex-col">
-                  <MoviesList movies={moviesList} />
-                  <button
-                    onClick={() => {
-                      setSize(size + 1);
-                      setIsInfiniteScrollActive(true);
-                    }}
-                    className="h-12 w-full text-3xl font-bold text-white bg-sky-500 my-4 rounded-xl"
-                  >
-                    Load more
-                  </button>
+                <div>
+                  {moviesList.length > 0 ? (
+                    <div className="flex flex-col">
+                      <MoviesList movies={moviesList} />
+                      {data[0].total_pages > size && (
+                        <button
+                          onClick={onPressNextPage}
+                          className="h-12 w-full text-3xl font-bold text-white bg-sky-500 my-4 rounded-xl"
+                        >
+                          Load more
+                        </button>
+                      )}
+                    </div>
+                  ) : (
+                    <div>No items were found that match your query.</div>
+                  )}
                 </div>
               )}
             </div>
